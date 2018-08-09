5b350a16f9571ec2da02cb0cd911d41206a14ec2
Tara Meyerink <tarajmeyerink@gmail.com>
2018-08-09 23:10:26 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,4 +11,20 @@ def assign_rooms(speakers)
   array = []
   room = 1 
   speakers.each do |speaker|
-    message = "Hello, #{speaker}! You'll be assigned to room #{
+    message = "Hello, #{speaker}! You'll be assigned to room #{room}!"
+    array.push(message)
+    room += 1 
+  end
+  array
+end
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def printer(attendees)
+  array_names = batch_badge_creator(attendees)
+  array_names.each do |name|
+    puts name
+  end
+  rooms = assign_rooms(attendees)
