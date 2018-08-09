8483f320c3b97b1a2174fdac9f93d8a48875869c
Tara Meyerink <tarajmeyerink@gmail.com>
2018-08-09 22:58:47 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,10 @@
-# Write your code here.
+def batch_badge_creator(names)
+  array_names = []
+  names.each do |name|
+    message = "Hello, my name is #{name}."
+    array_names.push(message)
+  end
+  array_names
+end
+
+def assign_rooms(speakers)
